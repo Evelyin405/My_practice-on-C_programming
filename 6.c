06604396a9075f6161bb7443d16fd1c3a97c6051
C. S. Evelyin Vishali <csevelyinvishali@gmail.com>
2026-02-19 19:49:13 +0530

    Added C Program to calculate product of three numbers

---
+++ 6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    const float PI = 3.14159;
+    float radius,area;
+    printf("Enter the radius of the circle:");
+    scanf("%f",&radius);
+    area = PI*radius*radius;
+    printf("Area of the circle = %.2f\n",area);
+    return 0;
+}
---
+++ product.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    float a,b,c,product;
+    scanf("%f %f %f",&a,&b,&c);
+    product = a*b*c;
+    printf("The product of three numbers is: %f\n",product);
+    return 0;
+
+}

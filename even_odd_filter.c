917408a375fc256eece9454ce6769502b134dfa5
C. S. Evelyin Vishali <csevelyinvishali@gmail.com>
2026-03-12 21:49:03 +0530

    Implement filtering of even and odd numbers

---
+++ even_odd_filter.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,e;
+    printf("Enter 5 numbers:\n");
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+    printf("Even Numbers are: ");
+    if(a % 2 == 0) printf("%d ", a);
+    if(b % 2 == 0) printf("%d ", b);
+    if(c % 2 == 0) printf("%d ", c);
+    if(d % 2 == 0) printf("%d ", d);
+    if(e % 2 == 0) printf("%d ", e);
+    printf("\nOdd Numbers are: ");
+    if(a % 2 != 0) printf("%d ", a);
+    if(b % 2 != 0) printf("%d ", b);
+    if(c % 2 != 0) printf("%d ", c);
+    if(d % 2 != 0) printf("%d ", d);
+    if(e % 2 != 0) printf("%d ", e);
+    return 0;
+}

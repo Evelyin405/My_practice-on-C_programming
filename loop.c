cbd9d6b371f88641669a66e5a6547d16c2cb5305
C. S. Evelyin Vishali <csevelyinvishali@gmail.com>
2026-03-12 21:29:15 +0530

    C program using for loop

---
+++ loop.c
@@ -0,0 +1,19 @@
+//Edit your code here
+#include<stdio.h>
+int main()
+{
+    int i,j,num = 1;
+    for(i = 1;i <= 3;i++)
+    {
+        for(j = 1;j <= 3;j++)
+        {
+            if(num % 2 == 0)
+                 printf("E");
+            else
+                 printf("O");
+            num++;         
+        }
+        printf("\n");
+    }
+    return 0;
+}

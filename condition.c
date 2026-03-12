113aa6b71a6968265f31ac8ceaa03e67ef959a4a
C. S. Evelyin Vishali <csevelyinvishali@gmail.com>
2026-03-12 21:18:53 +0530

    Cprogram to display thankyou message using if-else

---
+++ condition.c
@@ -0,0 +1,17 @@
+//Edit your code here
+#include<stdio.h>
+int main()
+{
+    int option;
+    printf("Enter option :");
+    scanf("%d", &option);
+    if(option == 1)
+    {
+        printf("Thank You");
+    }
+    else
+    {
+        printf("Try Again");
+    }
+    return 0;
+}
